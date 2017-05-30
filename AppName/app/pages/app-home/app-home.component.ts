5d03ee8df837119c0c4f7a382080c74b2bf3119b
guiech <gjaiquel@gmail.com>
2017-05-30 01:03:29 -0300

    home init

--- AppName/app/pages/app-home/app-home.component.ts
+++ AppName/app/pages/app-home/app-home.component.ts
@@ -1,15 +1,28 @@
+import application = require("application");
 import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-home',
 	moduleId: module.id,
-	templateUrl: './app-home/app-home.component.html',
-	styleUrls: ['./app-home/app-home.component.css']
+	templateUrl: 'app-home.component.html',
+	styleUrls: ['app-home-common.css', 'app-home.css']
 })
 
 export class AppHomeComponent implements OnInit {
 
+	isAndroid:boolean = false;
+
 	constructor() { }
 
-	ngOnInit() { }
+	ngOnInit() {
+		if(application.ios) {
+			this.isAndroid = false;
+		} else if (application.android) {
+			this.isAndroid = true;
+		}
+	}
+
+	showSideDrawer() {
+    	console.log("Show SideDrawer tapped.");
+	}
 }
